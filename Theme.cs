ef5503a243696761ad49516ecc3732f38fe32d47
BarbosOleg <lolipo.celio@gmail.com>
2025-05-17 18:17:19 +0300

    ...

---
+++ Theme.cs
@@ -0,0 +1,15 @@
+using System.Drawing;
+
+namespace Home_Cinema
+{
+    public class Theme
+    {
+        public string Name { get; set; }
+        public Color BackColor { get; set; }
+        public Color ForeColor { get; set; }
+        public Color ButtonBackColor { get; set; }
+        public Color ButtonForeColor { get; set; }
+        public Color PanelBackColor { get; set; }
+        public Font MainFont { get; set; }
+    }
+}
